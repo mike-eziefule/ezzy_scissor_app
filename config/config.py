181ffe10c6e3f68f5f488a13019c892d6ff631b0
mike-eziefule <mike.eziefule@gmail.com>
2024-04-01 08:26:51 +0100

    DOM for fag page2

--- config/config.py
+++ config/config.py
@@ -9,7 +9,7 @@ class Settings():
     db_url: str = "sqlite:///./Ezzy_Url_Shortener.sqlite"
 
     # default to SQLite
-    app_server: str = "local" #change to 'development' when hosting
+    app_server: str = "development" #change to 'development' when hosting
     
     #openai tags
     tags = [
